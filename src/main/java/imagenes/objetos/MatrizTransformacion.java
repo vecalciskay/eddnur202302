8a0ae5b458010c35e672c7b7f7e13e90313acea4
vecalciskay <vladimir.calderon@gmail.com>
2023-09-05 08:41:55 -0400

    Matriz de transformacion completa

--- src/main/java/imagenes/objetos/MatrizTransformacion.java
+++ src/main/java/imagenes/objetos/MatrizTransformacion.java
@@ -1,22 +1,26 @@
 package imagenes.objetos;
 
+import java.text.DecimalFormat;
+
 public class MatrizTransformacion {
-    private double[][] matriz;
+    private final double[][] matriz;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     public MatrizTransformacion() {
         matriz = new double[3][3];
 
-        matriz[0][0] = 1;
-        matriz[0][1] = 0;
-        matriz[0][2] = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j)
+                    matriz[i][j] = 1.0;
+                else
+                    matriz[i][j] = 0.0;
+            }
+        }
+    }
 
-        matriz[1][0] = 0;
-        matriz[1][1] = 1;
-        matriz[1][2] = 0;
-
-        matriz[2][0] = 0;
-        matriz[2][1] = 0;
-        matriz[2][2] = 1;
+    public MatrizTransformacion(double[][] m) {
+        matriz = m;
     }
 
     public Vector2 aplicarMatriz(Vector2 xy) {
@@ -39,16 +43,77 @@ public class MatrizTransformacion {
         return new Vector2(resultado[0], resultado[1]);
     }
 
-    public void rotacion(int grados) {
+    public MatrizTransformacion rotacion(int grados) {
         double r=grados*Math.PI/180.0;
-        matriz[0][0] = Math.cos(r);
-        matriz[0][1] = Math.sin(r);
-        matriz[1][0] = -Math.sin(r);
-        matriz[1][1] = Math.cos(r);
+        MatrizTransformacion m1 = new MatrizTransformacion();
+        m1.set(0,0, Math.cos(r));
+        m1.set(0,1, Math.sin(r));
+        m1.set(1,0, -Math.sin(r));
+        m1.set(1, 1, Math.cos(r));
+
+        return componer(m1);
     }
 
-    public void traslacion(int x, int y) {
-        matriz[0][2] = (double)x;
-        matriz[1][2] = (double)y;
+    public MatrizTransformacion traslacion(int x, int y) {
+        MatrizTransformacion m1 = new MatrizTransformacion();
+        m1.set(0,2, x);
+        m1.set(1,2, y);
+
+        return componer(m1);
+    }
+
+    public MatrizTransformacion escala(double x, double y) {
+        MatrizTransformacion m1 = new MatrizTransformacion();
+        m1.set(0,0, x);
+        m1.set(1,1, y);
+
+        return componer(m1);
+    }
+
+    public MatrizTransformacion componer(MatrizTransformacion m1) {
+        double[][] m2 = new double[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    m2[i][j] += m1.get(i,k) * this.get(k,j);
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matriz[i][j] = m2[i][j];
+            }
+        }
+        return this;
+    }
+
+    public double get(int i, int j) {
+        return matriz[i][j];
+    }
+    public void set(int i, int j, double valor) {
+        matriz[i][j] = valor;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder resultado = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            String separador = "";
+            for (int j = 0; j < 3; j++) {
+                String valor = df.format(matriz[i][j]);
+                resultado.append(separador).append(valor);
+                separador = "  ";
+            }
+            resultado.append("\n");
+        }
+        return resultado.toString();
+    }
+
+    public void copiar(MatrizTransformacion m) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matriz[i][j] = m.get(i,j);
+            }
+        }
     }
 }
