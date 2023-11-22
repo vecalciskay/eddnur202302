c578d0348c00386bca6037f41c845b628221ba06
Nahim Terrazas Parada <nahimdhaney@hotmail.com>
2023-11-22 19:45:03 -0400

    clean code y descripciones

--- src/main/java/grafos/DijkstraAlgoritmo.java
+++ src/main/java/grafos/DijkstraAlgoritmo.java
@@ -1,42 +1,35 @@
 package grafos;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class DijkstraAlgoritmo {
 
     // creo el metodo de dijkstra que recibe un Grafo, un nodo Origen y un Nodo Destino
     public static void Dijkstra(Grafo g, Grafo.Nodo verticeInicial, Grafo.Nodo verticeDestino) {
-        HashMap<String, Integer> nodoIdYacumulado = new HashMap<>();
-        List<String> listaIdNodosARecorrer = new ArrayList<>();
-        HashMap<String, String> predecesores = new HashMap<>();
-        List<String> recorridos = new ArrayList<>();
-        HashMap<String, List<String>> listaDeMejorRutas = new HashMap<>();
+        HashMap<String, Integer> resultados = new HashMap<>(); // guardaré los nodos con su menor costo
+        List<String> nodosPendientesRecorrer = new ArrayList<>(); // guardaré los nodos que me faltan por recorrer
+        HashMap<String, String> predecesores = new HashMap<>();  // guardaré los nodos predecesores de cada nodo
 
+        resultados.put(verticeInicial.getId(), 0);
+        nodosPendientesRecorrer.add(verticeInicial.getId());
 
-        nodoIdYacumulado.put(verticeInicial.getId(), 0);
-        recorridos.add(verticeInicial.getId());
-        listaIdNodosARecorrer.add(verticeInicial.getId());
-
-        while (!listaIdNodosARecorrer.isEmpty()) {
-            String nodoConMenorValor = obtenerNodoConMenorValor(listaIdNodosARecorrer, nodoIdYacumulado);
-            listaIdNodosARecorrer.remove(nodoConMenorValor);
+        while (!nodosPendientesRecorrer.isEmpty()) { // hasta que no tenga nodos pendientes por recorrer
+            String nodoConMenorValor = obtenerNodoConMenorValor(nodosPendientesRecorrer, resultados); // obtengo el nodo con menor valor
+            nodosPendientesRecorrer.remove(nodoConMenorValor); // lo remuevo de los nodos pendientes por recorrer
 
             Grafo.Nodo nodoActual = g.obtenerNodo(nodoConMenorValor);
 
             for (Object aristaObj : nodoActual.getSalientes()) {
                 Grafo.Arista arista = (Grafo.Arista) aristaObj;
                 Grafo.Nodo nodoDestino = arista.getHacia();
-                int nuevoAcumulado = nodoIdYacumulado.get(nodoConMenorValor) + arista.getPeso();
+                int nuevoAcumulado = resultados.get(nodoConMenorValor) + arista.getPeso();
 
-                if (!nodoIdYacumulado.containsKey(nodoDestino.getId()) || nuevoAcumulado < nodoIdYacumulado.get(nodoDestino.getId())) {
-                    nodoIdYacumulado.put(nodoDestino.getId(), nuevoAcumulado);
-                    listaIdNodosARecorrer.add(nodoDestino.getId());
-                    predecesores.put(nodoDestino.getId(), nodoConMenorValor);
+                if (!resultados.containsKey(nodoDestino.getId()) || nuevoAcumulado < resultados.get(nodoDestino.getId())) {
+                    resultados.put(nodoDestino.getId(), nuevoAcumulado); // adicionamos si no existe o es menor
+                    predecesores.put(nodoDestino.getId(), nodoConMenorValor); // adicionamos si no existe o es menor
+                    nodosPendientesRecorrer.add(nodoDestino.getId());
                 }
             }
-
-            recorridos.add(nodoConMenorValor);
         }
         // Reconstruir el camino desde verticeDestino hasta verticeInicial
         List<String> recorrido = new ArrayList<>();
@@ -85,3 +78,11 @@ public class DijkstraAlgoritmo {
         Dijkstra(grafo,a,h);
     }
 }
+// Santa Cruz - Ivirgazama 256
+// Ivirgazama - Cochabamba 226
+// Cochabamba - Oruro 215
+// Oruro - Sucre 236
+// Sucre - Aiquile 138
+// Santa Cruz - Aiquile 344
+// Santa Cruz - Vallegrande 250
+// Vallegrande - Sucre 350
